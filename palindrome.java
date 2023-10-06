d99ef15556d2e21473b02e232f98ac6daf1c9143
satyam <satyampayal10@gmail.com>
2023-10-06 19:34:21 +0530

    Java questions for begginers

--- palindrome.java
+++ palindrome.java
@@ -5,6 +5,19 @@ public class palindrome {
         System.out.println("Enter the number for print Fibonnaci series");
          int p;
          p=sc.nextInt();
-         
+         int temp=p;
+         int sum=0;
+         while(p>0){
+            int digit=p%10;
+            sum=digit+sum*10;
+            p/=10;
+         }
+         if(sum==temp){
+            System.out.println("Yes its Palindrom"+sum);
+         }
+         else{
+            System.out.println("No its not"+sum);
+         }
+         sc.close();
     }
 }
